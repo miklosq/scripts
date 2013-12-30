f82f5d52d5eca36ee7a290859753237ec38c74f3
Miklos Quartus <inbox@miklos.info>
2013-12-30 21:35:51 +0000

    made it a one-liner

--- passgen.sh
+++ passgen.sh
@@ -1,3 +1,1 @@
-dd if=/dev/urandom bs=256 count=1 2>/dev/null |uuencode -m - | tail -n2 | tr -dc '0-9a-zA-Z' | head -c 8
-echo
-exit 0 
+( dd if=/dev/urandom bs=256 count=1 2>/dev/null |uuencode -m - | tail -n2 | tr -dc '0-9a-zA-Z' | head -c 8 ); echo ""
